570dd1c9a7b8de1532f4925a7b6103b2d80260b0
Adity Srivastava <adisrivastava848@gmail.com>
2026-07-23 14:43:53 +0530

    Fix Vercel API build error

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     const posts = await prisma.post.findMany({
